960ae615dd76a66235101389cd85fc7af0a6c256
RhysyPiece360 <67196752+RhysyPiece360@users.noreply.github.com>
2022-01-09 00:55:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const winston = require('winston');
 var app = express()
 const port = 8080
 const context =("request handeld ")
+const io = require('@pm2/io');
+const fs = require('fs');
 //bruh, my lint got mad at me, when i was reading off code from docs lmfao
 const logger = winston.createLogger({
     transports: [
@@ -14,16 +16,24 @@ const logger = winston.createLogger({
     level: 'info',
     message: 'api start'
   });
-   
+  //pm2 stuff
+ 
+
+  const currentReqs = io.counter({
+    name: 'Realtime request count',
+    id: 'app/realtime/requests'
+  });
+  
 app.get('/', function (req, res) {
     console.log("request required")
-    res.send('hello world')
+    res.send('/image endpoint has the actuall images sent,')
     
     console.log("Request Finished")
     logger.info('api request handeld');
+    currentReqs.inc();
   })
   const path = require('path');
-const fs = require('fs');
+
 
 const directoryPath = path.join(__dirname, 'image');
 const pickRandomNumber = (max) => Math.floor(Math.random() * max);
@@ -40,14 +50,20 @@ app.get('/image', function (req, res) {
         const pickedIndex = pickRandomNumber(images.length);
         const imageName = images[pickedIndex];
         res.sendFile(path.join(directoryPath, imageName));
+        logger.info('api request handeld');
+        currentReqs.inc();
     });
 });
+app.get('/imagecount', function (req, res) {
+res.send("in devlopment still")
 
+})
 console.log("works here")
 // i apparently dont remember any syntax of javascript, thats nice
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`app listening at http://satawiki.catsarecute.xyz:${port}`)
   })
+  
   
   
