372fd5cb0ba213f1011d1cf33f878aaecc3c91ec
RhysyPiece360 <67196752+RhysyPiece360@users.noreply.github.com>
2022-01-29 19:50:32 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 8080
 const context =("request handeld ")
 const io = require('@pm2/io');
 const fs = require('fs');
+const cissystem = require('cis-system')
 //bruh, my lint got mad at me, when i was reading off code from docs lmfao
 const logger = winston.createLogger({
     transports: [
@@ -13,7 +14,7 @@ const logger = winston.createLogger({
     ]
   });
   logger.log({
-    level: 'info',
+    level: 'start',
     message: 'api start'
   });
   //pm2 stuff
@@ -26,8 +27,7 @@ const logger = winston.createLogger({
   
 app.get('/', function (req, res) {
     console.log("request required")
-    res.send('/image endpoint has the actuall images sent,')
-    
+    res.send('Invalid endpoint. Please go to /endpoints')
     console.log("Request Finished")
     logger.info('api request handeld');
     currentReqs.inc();
@@ -41,7 +41,7 @@ const pickRandomNumber = (max) => Math.floor(Math.random() * max);
 const allowedFileTypes = [".jpg", ".png"];
 const hasAllowedEnding = file => allowedFileTypes.some(ending => file.toLowerCase().endsWith(ending));
 
-app.get('/image', function (req, res) {
+app.get('/animal/catimg', function (req, res) {
     fs.readdir(directoryPath, function (err, files) {
         if (err) {
             return console.log('Unable to scan directory: ' + err);
@@ -50,20 +50,61 @@ app.get('/image', function (req, res) {
         const pickedIndex = pickRandomNumber(images.length);
         const imageName = images[pickedIndex];
         res.sendFile(path.join(directoryPath, imageName));
-        logger.info('api request handeld');
+        logger.info('api request handeld for cot image');
         currentReqs.inc();
     });
 });
+
+const birddirectoryPath = path.join(__dirname, 'birdimg');
+const birdpickRandomNumber = (max) => Math.floor(Math.random() * max);
+
+const birballowedFileTypes = [".jpg", ".png"];
+const birbhasAllowedEnding = file => allowedFileTypes.some(ending => file.toLowerCase().endsWith(ending));
+
+app.get('/animal/birdimg', function (req, res) {
+    fs.readdir(birddirectoryPath, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        const images = files.filter(hasAllowedEnding);
+        const pickedIndex = birdpickRandomNumber(images.length);
+        const imageName = images[pickedIndex];
+        res.sendFile(path.join(birddirectoryPath, imageName));
+        logger.info('api request handeld for birb img');
+        currentReqs.inc();
+    });
+});
+
 app.get('/imagecount', function (req, res) {
-res.send("in devlopment still")
+
+  fs.readdir(directoryPath, (err, files) => {
+    res.send(files.length)
+    logger.info('api request handeld');
+    currentReqs.inc();
+  });
+
 
 })
+
+
+app.get('/endpoints', function (req, res) {
+ res.send("Current endpoints, /animals /animal/birdimg, /animal/catimg")
+ logger.info('api request handeld');
+        currentReqs.inc();
+})
+
+app.get('/animals', function (req, res) {
+  res.send("Curent animals On the api, Cat images, and Bird images ")
+  logger.info('api request handeld');
+         currentReqs.inc();
+ })
+ 
+
 console.log("works here")
 // i apparently dont remember any syntax of javascript, thats nice
 
 app.listen(port, () => {
-    console.log(`app listening at http://satawiki.catsarecute.xyz:${port}`)
+    console.log(`app listening at http://localhost:${port}`)
   })
-  
   
   
